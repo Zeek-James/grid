c7c68f240caa36b9ef54dcb3550068ae2162c913
Isaac James <chizuruokej@yahoo.com>
2022-07-23 14:49:51 +0100

    new updates on grid area

--- src/components/GridArea.jsx
+++ src/components/GridArea.jsx
@@ -3,12 +3,12 @@ import React from "react";
 export const GridArea = () => {
   return (
     <div className="ga__Container">
-      <div className="one">1</div>
-      <div className="two">2</div>
-      <div className="three">3</div>
-      <div className="four">4</div>
-      <div className="five">5</div>
-      <div className="six">6</div>
+      <header>Header</header>
+      <main>Main</main>
+      <section>Section</section>
+      <nav>Nav</nav>
+      <aside>Aside</aside>
+      <footer>Footer</footer>
     </div>
   );
 };
